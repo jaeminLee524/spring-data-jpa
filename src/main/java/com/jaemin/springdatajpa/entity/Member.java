e6e05678cac47d44d1d504634c0fed67beb97cd2
jaeminLee524 <rkwoals524@hanmail.net>
2022-03-03 08:33:17 +0900

    create member domain

--- src/main/java/com/jaemin/springdatajpa/entity/Member.java
+++ src/main/java/com/jaemin/springdatajpa/entity/Member.java
@@ -1,24 +1,42 @@
 package com.jaemin.springdatajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String username;
+    private int age;
 
-    protected Member(){}
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            this.team = team;
+        }
+    }
+
+    //연관관계 생성 메서드
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMemberList().add(this);
     }
 }
